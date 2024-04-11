1f58edd7bf6bef2f64d4f8d66fc01df9f1645498
karthikreddy005 <117522702+karthikreddy005@users.noreply.github.com>
2024-04-11 23:12:59 +0530

    polymorphism first attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        Product p = new Product();
+        int ans = p.product(3,5);
+        int ans1 = p.product(3,5,7);
+        double ans2 = p.product(2.0 , 5.0);
+        System.out.println(ans+" "+ans1+" "+ans2);
+
+    }
+    public static class product{
+        public int product(int x , int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 
 }
